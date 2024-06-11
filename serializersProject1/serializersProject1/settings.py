e89c8497c75709a0f1f55371292ec0274aadde51
yeonnan <145152442+yeonnan@users.noreply.github.com>
2024-06-11 14:40:39 +0900

    serializersProject

--- serializersProject1/serializersProject1/settings.py
+++ serializersProject1/serializersProject1/settings.py
@@ -135,9 +135,11 @@ AUTH_USER_MODEL = 'accounts.User'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 2,
 }
 
 
