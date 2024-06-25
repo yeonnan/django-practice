b70822da477e0504b05ac35f781e3be12af2b382
yeonnan <145152442+yeonnan@users.noreply.github.com>
2024-06-26 02:39:42 +0900

    django serialize

--- api_pjt/articles/views.py
+++ api_pjt/articles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.core import serializers
 from .models import Article
 
 
@@ -25,3 +26,11 @@ def json_01(request):
     # safe : json_articles에 들어간게 dict이면 safe=False를 안적어줘도 된다.
     # 지금은 [], list가 들어있기 때문에 safe=False로 적어주면 내부적으로 처리해서 JsonResponse를 돌려준다.
     return JsonResponse(json_articles, safe=False)  
+
+
+def json_02(request):
+    articles = Article.objects.all()
+    # jsno 포맷으로, articles 데이터로 직렬화
+    res_data = serializers.serialize('json', articles)
+    # content_type : Response 구조 중에 정보로 들어있다.
+    return HttpResponse(res_data, content_type='application/json')
