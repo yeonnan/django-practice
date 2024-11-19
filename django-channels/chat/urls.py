cf258d4e9d8c85040173d12f4dec94c8936bdb40
yeonnan <145152442+yeonnan@users.noreply.github.com>
2024-11-20 04:05:32 +0900

    채팅방 생성, 수정, 목록, 상세페이지

--- django-channels/chat/urls.py
+++ django-channels/chat/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.role_playing_room_list, name='role_playing_room_list'),
     path('new/', views.role_playing_room_new, name='role_playing_room_new'),
     path('<int:pk>/edit/', views.role_playing_room_edit, name='role_playing_room_edit'),
+    path('<int:pk>/', views.role_playing_room_detail, name='role_playing_room_detail'),
+    path('<int:pk>/delete/', views.role_playing_room_delete, name='role_playing_room_delete'),
+    
 ]
--- django-channels/chat/views.py
+++ django-channels/chat/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView, DetailView, DeleteView
+from django.views.generic.list import ListView
 from .models import RolePlayingRoom
 from .forms import RolePlayingRoomForm
 from django.utils.decorators import method_decorator
 from django.contrib.admin.views.decorators import staff_member_required
+from django.urls import reverse_lazy
+from django.contrib import messages
+
+
+@method_decorator(staff_member_required, name="dispatch")
+class RolePlaylingRoomListAPIView(ListView):
+    model = RolePlayingRoom
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.filter(user=self.request.user)
+        return qs
+    
+role_playing_room_list = RolePlaylingRoomListAPIView.as_view()
+
+
+@method_decorator(staff_member_required, name="dispatch")
+class RolePlaylingRoomDetailAPIView(DetailView):
+    model = RolePlayingRoom
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.filter(user=self.request.user)
+        return qs
+    
+role_playing_room_detail = RolePlaylingRoomDetailAPIView.as_view()
 
 
 @method_decorator(staff_member_required, name="dispatch")
@@ -28,3 +55,19 @@ class RolePlayingRoomUpdateView(UpdateView):
         qs = qs.filter(user=self.request.user)
         return qs
 role_playing_room_edit = RolePlayingRoomUpdateView.as_view()
+
+
+@method_decorator(staff_member_required, name="dispatch")
+class RolePlayingRoomDeleteView(DeleteView):
+    model = RolePlayingRoom
+    success_url = reverse_lazy("role_playing_room_list")
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.filter(user=self.request.user)
+        return qs
+    
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, "채팅방을 삭제했습니다.")
+        return response
+role_playing_room_delete = RolePlayingRoomDeleteView.as_view()
