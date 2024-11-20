41f54fda28d202da6f84eac5ccce6d89c942e716
yeonnan <145152442+yeonnan@users.noreply.github.com>
2024-11-20 11:29:59 +0900

    웹소켓 통신 - 클라이언트, 값 유지

--- django-channels/chat/consumers.py
+++ django-channels/chat/consumers.py
@@ -2,7 +2,13 @@ from channels.generic.websocket import JsonWebsocketConsumer
 
 
 class RolePlayingRoomConsumer(JsonWebsocketConsumer):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.count = 0
+
     
     def receive_json(self, content, **kwargs):
-        print('received : ', content)
+        self.count += 1
+        content['count'] = self.count
         self.send_json(content)
--- django-channels/djangoChannels/asgi.py
+++ django-channels/djangoChannels/asgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLResolver
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoChannels.settings')
 
@@ -20,5 +20,5 @@ import chat.routing
 
 application = ProtocolTypeRouter({
     'http' : django_asgi_app,
-    'websocket' : URLResolver(chat.routing.websocket_urlpatterns),
+    'websocket' : URLRouter(chat.routing.websocket_urlpatterns),
 })
